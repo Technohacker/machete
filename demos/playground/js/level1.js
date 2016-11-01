3889e13a080e4b6c4da234e5ea9fc9386578b434
Technohacker <technohacker1995@gmail.com>
2016-11-01 23:09:36 +0530

    Playground: Stop unlimited jumping and add world bounds

--- demos/playground/js/level1.js
+++ demos/playground/js/level1.js
@@ -14,6 +14,7 @@
         element: Machete.querySelector("#level1"),
         init() {
             this.player = new Sprite(document.querySelector("#player"), function () {
+                    this.jumpCount = 0;
                     let keys = KeyboardInput.keys;
 
                     KeyboardInput.registerInputHandlers([{
@@ -32,10 +33,15 @@
                         keyUp: (event) => {}
                     }, {
                         key: keys.SPACE,
-                        keyDown: (event) => this.applyForce({
-                            x: 0,
-                            y: -0.01
-                        }),
+                        keyDown: (event) => {
+                            if (this.jumpCount < 2) {
+                                this.applyForce({
+                                    x: 0,
+                                    y: -0.01
+                                });
+                                this.jumpCount += 1;
+                            }
+                        },
                         keyUp: (event) => {}
                     }]);
                 },
@@ -46,7 +52,8 @@
                     width: 20,
                     height: 20,
                     options: {
-                        density: 100
+                        label: "Player",
+                        restitution: 0.8
                     }
                 });
             this.engine = Engine.create({
@@ -58,17 +65,49 @@
                     }
                 })
             });
+            Matter.Events.on(this.engine, "collisionActive", event => {
+                if (event.pairs[0].bodyA.label === "Player" || event.pairs[0].bodyB.label === "Player") {
+                    this.player.jumpCount = 0;
+                }
+            });
 
             World.add(this.engine.world, [this.player.rigidBody]);
 
-            let position = Machete.getOffsetPosition(document.querySelector("#wall1"));
+            let wallBounds = [
+                Machete.getOffsetPosition(document.querySelector("#ground")),
+                Machete.getOffsetPosition(document.querySelector("#wall-left")),
+                Machete.getOffsetPosition(document.querySelector("#wall-right")),
+                Machete.getOffsetPosition(document.querySelector("#ceiling"))
+            ]
 
             var wallBodies = [
-                Bodies.rectangle(position.left, position.top, position.width, position.height, {
+                Bodies.rectangle(wallBounds[0].left, wallBounds[0].top, wallBounds[0].width, wallBounds[0].height, {
                     isStatic: true,
-                    element: document.querySelector("#wall1"),
-                    width: position.width,
-                    height: position.height
+                    element: document.querySelector("#ground"),
+                    width: wallBounds[0].width,
+                    height: wallBounds[0].height,
+                    restitution: 1
+                }),
+                Bodies.rectangle(wallBounds[1].left, wallBounds[1].top, wallBounds[1].width, wallBounds[1].height, {
+                    isStatic: true,
+                    element: document.querySelector("#wall-left"),
+                    width: wallBounds[1].width,
+                    height: wallBounds[1].height,
+                    restitution: 1
+                }),
+                Bodies.rectangle(wallBounds[2].left, wallBounds[2].top, wallBounds[2].width, wallBounds[2].height, {
+                    isStatic: true,
+                    element: document.querySelector("#wall-right"),
+                    width: wallBounds[2].width,
+                    height: wallBounds[2].height,
+                    restitution: 1
+                }),
+                Bodies.rectangle(wallBounds[3].left, wallBounds[3].top, wallBounds[3].width, wallBounds[3].height, {
+                    isStatic: true,
+                    element: document.querySelector("#ceiling"),
+                    width: wallBounds[3].width,
+                    height: wallBounds[3].height,
+                    restitution: 0
                 })
             ];
 
